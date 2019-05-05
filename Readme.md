517c5aecee3954efa2a18e515e5efba7371d2d0f
xinliqun <418618239@qq.com>
2019-05-05 14:27:17 +0800

    02: html静态模版

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
-* npm init 
-* 善用"start": "node server/index.js",
+### 初始化项目
+    * npm init 
+    * 善用"start": "node server/index.js",
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const koa = require('Koa')
 const app = new koa()
+const {normal} = require('./tpl')
 
 app.use(async (ctx, next) => {
-    ctx.body = "movie"
+    ctx.type = "text/html; chartset=utf-8"
+    ctx.body = normal
 })
 
 app.listen(45555)
